eaf6f7cbdaf851b7f724637016a2c6f8a6aa18a4
Shaista Shabbir <shaista.shabbir@hitec-hamburg.de>
2025-03-28 17:50:54 +0100

    handle missing values

--- etl_pipeline/src/transform.py
+++ etl_pipeline/src/transform.py
@@ -1,18 +1,21 @@
 import pandas as pd
 from loguru import logger
 
+
 def clean_data(df):
     """Cleans and transforms the data."""
     try:
         # Standardize date format
-        df["date_of_birth"] = pd.to_datetime(df["date_of_birth"], errors="coerce").dt.strftime("%Y-%m-%d")
+        df["date_of_birth"] = pd.to_datetime(
+            df["date_of_birth"], errors="coerce"
+        ).dt.strftime("%Y-%m-%d")
 
         # Remove duplicates
         df = df.drop_duplicates()
 
         # Handle missing values
         df = df.dropna(subset=["name", "date_of_birth"])
-
+        df["salary"] = df["salary"].fillna(0)
         # Convert salary to numeric
         df["salary"] = pd.to_numeric(df["salary"], errors="coerce")
 
